458a4506260e9c5e8b0e95f43ad9d3c0f6c8088b
user907306 <user907306@eu242.local>
2018-07-29 01:00:41 +0100

    Initial Commit

---
+++ Flash Chat/OtherUserCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  OtherUserCellTableViewCell.swift
+//  Flash Chat
+//
+//  Created by user907306 on 6/10/18.
+//  Copyright © 2018 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class OtherUserCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
